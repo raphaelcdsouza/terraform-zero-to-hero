ed4f5003660871f0a6dcc7d1b7f5a75fbab88d7b
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2024-04-19 14:26:45 -0700

    condition expressions

---
+++ 24_CONDITIONAL_EXPRESSIONS_AZURE_STORAGE_ACCOUNT/variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  description = "Region where the resources will be created in Azure"
+  type        = string
+  default     = "Canada Central"
+}
+
+variable "environment" {
+  description = "Environment where the resources will be created in Azure"
+  type        = string
+}
