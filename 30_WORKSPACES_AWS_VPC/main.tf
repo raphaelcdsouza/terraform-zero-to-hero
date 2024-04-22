b6cdd84a17a81f3b551a0842fb3ede48f090de9b
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2024-04-22 11:11:27 -0700

    terraform workspaces

---
+++ 30_WORKSPACES_AWS_VPC/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.45.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "raphaelcdsouza-remote-state"
+    key    = "workspaces/terraform.tfstate"
+    region = "ca-central-1"
+  }
+}
+
+provider "aws" {
+  region = "ca-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "raphaelcdsouza"
+      managed-by = "terraform"
+    }
+  }
+}
